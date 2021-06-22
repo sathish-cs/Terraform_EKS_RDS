25cada5bcaae7f633a485d0194d76d5bf2df458c
sathish-cs <rockersathish@hotmail.com>
2021-06-22 11:54:08 +0530

    production resources

---
+++ production/main.tf
@@ -0,0 +1,48 @@
+module "vpc" {
+  source               = "../modules/vpc/"
+  vpc_cidr             = var.vpc_cidr
+  public_cidr          = var.public_cidr
+  private_cidr         = var.private_cidr
+  avail_zone           = var.avail_zone
+  public_subnet_names  = var.public_subnet_names
+  private_subnet_names = var.private_subnet_names
+  vpc_id               = module.vpc.vpc
+  tags                 = var.tags
+}
+
+module "rds" {
+  source                  = "../modules/rds/"
+  port                    = var.port
+  name                    = var.name
+  cluster_name            = var.cluster_name
+  instance_class          = var.instance_class
+  db                      = var.db
+  username                = var.username
+  password                = var.password
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  deletion_protection     = var.deletion_protection
+  backup_retention_period = var.backup_retention_period
+  apply_immediately       = var.apply_immediately
+  backup_window           = var.backup_window
+  final_snapshot          = var.final_snapshot
+  public_subnet           = module.vpc.public_subnet[0]
+  rds_subnet              = module.vpc.private_subnet
+  vpc                     = module.vpc.vpc
+  tags = var.tags
+}
+
+module "eks" {
+  source              = "../modules/eks_cluster/"
+  vpc                 = module.vpc.vpc
+  public_subnet       = module.vpc.public_subnet
+  private_subnet      = module.vpc.private_subnet
+  eks-cluster-name    = "EKS-Cluster-Production"
+  cluster_role_name   = "EKS-Role-Production"
+  node_role_name      = "EKS-Node-Role-Production"
+  node_group_name     = "EKS-Workers-Production"
+  security_group_name = "EKS-Cluster-Production"
+  key                 = module.rds.ssh_key
+  security_group      = module.rds.bastion_security_group
+  tags                = var.tags
+}
