625e7f396bcc1a50cbb95beeb915fea2cc168eee
sathish-cs <rockersathish@hotmail.com>
2021-06-22 17:48:28 +0530

    Updated variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -15,7 +15,7 @@ variable "private_cidr" {
 
 variable "avail_zone" {
   type    = list(any)
-  default = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  default = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
 }
 
 variable "public_subnet_names" {
