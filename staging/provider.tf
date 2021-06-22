ad686e75eed8b841b50175bcca1f24321c854cf8
sathish-cs <rockersathish@hotmail.com>
2021-06-22 11:54:17 +0530

    staging resources

---
+++ staging/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.46.0"
+    }
+  }
+
+  required_version = ">= 0.15.5" # Terraform version should be greater than or equal to this
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+  profile = "hashi"
+}
